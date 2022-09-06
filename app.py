3682250ed996ffc120d7eb97f5f6aad6bbe6e21a
jeffersonjpr <jefferson_michael407@hotmail.com>
2022-09-05 23:26:25 -0300

    routing

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return 'Hello World!'
 
-@app.route('/greet')
+@app.route('/hello')
 def greet():
-    return 'Hello there!'
+    return 'Hello, good morning!'
+
+
+#variables
+@app.route('/user/<username>')
+def show_username(username):
+    #return 'User %s' % username
+    return 'User {}'.format(username)
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    #return 'Post %d' % post_id
+    return 'Post {}'.format(post_id)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return 'POST'
+    else:
+        return 'GET'
