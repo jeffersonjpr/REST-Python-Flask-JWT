f0c6093d52141d8810ced1160f4a7d8bb7019613
jeffersonjpr <jefferson_michael407@hotmail.com>
2022-09-10 15:30:30 -0300

    leftover

--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, request
-app = Flask(__name__)
-
-@app.route('/user', methods=['GET', 'POST'])
-def login():
-    username = request.args.get('username')
-    password = request.args.get('password')
-    print(username, password)
-    return 'username: '
